ce952f3aad3411a7df860b48b04aba1c687b22a8
Incyaa <incyaa@outlook.com>
2024-07-03 09:23:17 +0300

    Initial commit

---
+++ Keep up the hoop/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Keep_up_the_hoop;
+
+class Program
+{
+    public static string HoopCount(int n)
+    {
+        //Your code goes here
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
